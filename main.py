91a19ac57473cf00b88280b7d468b2b045aa42d6
d-chandrahas <chandrahasreddy732@gmail.com>
2023-05-23 20:56:23 +0530

    updated todo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ i = 0
 
 while True:
 
-    reqUrl = "https://prnt.sc/" + ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+    reqUrl = "https://prnt.sc/" + ''.join(random.choices(string.digits + string.ascii_lowercase, k=6))
 
     ls_res = requests.get(reqUrl,  headers={"Accept": "*/*", "User-Agent": "Python 3"})
     i += 1
@@ -35,8 +35,10 @@ while True:
         print(i, img_url)
         img = Image.open(img_host_res.raw)
         # img.show()
+
         # plt.imshow(img)
         # plt.show()
+
         cv2.imshow('image', cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR))
         cv2.waitKey(2000)
 
