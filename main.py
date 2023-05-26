46b054aa2bfb13cf087271b10a6e8658a31654b3
d-chandrahas <chandrahasreddy732@gmail.com>
2023-05-26 17:42:18 +0530

    made functions and better filters

--- main.py
+++ main.py
@@ -8,27 +8,34 @@ import numpy as np
 import cv2
 
 
+IMG_URL_REGEX = re.compile(r'<meta property="og:image" content="(.*?)"/>')
+
+
+def get_img_url(ls_res):
+    img_url = IMG_URL_REGEX.search(ls_res.text)
+    return None if img_url is None else img_url.group(1)
+
+def filter_img_url(img_url):
+    if img_url is None : return None
+    if img_url == "//st.prntscr.com/2023/05/26/0610/img/0_173a7b_211be8ff.png" : return None
+    if img_url.startswith("https://image.prntscr.com") : return None
+    return img_url
+
 i = 0
 
 while True:
 
-    reqUrl = "https://prnt.sc/" + ''.join(random.choices(string.digits + string.ascii_lowercase, k=6))
+    ls_url = "https://prnt.sc/" + ''.join(random.choices(string.digits + string.ascii_lowercase, k=6))
+    # ls_url = "https://prnt.sc/" + f'{i}'
 
-    ls_res = requests.get(reqUrl,  headers={"Accept": "*/*", "User-Agent": "Python 3"})
+    ls_res = requests.get(ls_url,  headers={"Accept": "*/*", "User-Agent": "Python 3"})
     i += 1
-    # img_url = ls_res.text.split("<meta property=\"og:image\" content=\"")[1].split("\"")[0]
-    # print(img_url)
 
-    img_url = re.search(r"https://i\.imgur\.com/[a-zA-Z0-9]{7}\.(png|jpg|jpeg)", ls_res.text)
+    img_url = filter_img_url(get_img_url(ls_res))
+    # print(ls_url, img_url)
+    # continue
 
-    if img_url is None:
-        continue
-        img_url = re.search(r"https://image\.prntscr\.com/image/([a-zA-Z0-9_\-]{22}|[a-zA-Z0-9_\-]{32})\.(png|jpg|jpeg)", ls_res.text)
-
-        if img_url is None:
-            continue
-
-    img_url = img_url.group()
+    if img_url is None: continue
 
     img_host_res = requests.get(img_url, stream=True)
     if img_host_res.status_code == 200:
