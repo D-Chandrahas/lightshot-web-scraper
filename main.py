ab417205a311b49b75c421790cda03daec5e51a9
d-chandrahas <chandrahasreddy732@gmail.com>
2023-06-07 20:05:07 +0530

    updated headers, logging, refactoring, catch sigint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import sys
 import requests
 import random
 import string
@@ -9,15 +10,21 @@ import numpy as np
 import cv2
 
 
+HEADERS = {
+    "User-Agent": "Python 3",
+    "Connection": "keep-alive"
+}
+
 IMGUR_ERR_IMAGE = requests.get("https://i.imgur.com/removed.png").content
 
 IMG_URL_REGEX = re.compile(r'<img class="no-click screenshot-image" src="(.*?)"')
 
+
 def random_ls_url(length=6):
     return "https://prnt.sc/" + random.choices("123456789" + string.ascii_lowercase)[0] + "".join(random.choices(string.digits + string.ascii_lowercase, k=length-1))
 
-def get_img_url(ls_res):
-    img_url = IMG_URL_REGEX.search(ls_res.text)
+def get_img_url(ls_res_body):
+    img_url = IMG_URL_REGEX.search(ls_res_body)
     return None if img_url is None else img_url.group(1)
 
 def filter_img_url(img_url):
@@ -26,48 +33,57 @@ def filter_img_url(img_url):
     # if img_url.startswith("https://image.prntscr.com") : return True
     return False
 
-
 def filter_img(img):
     if img == IMGUR_ERR_IMAGE : return True
     return False
-    
 
-i = 0
+ 
+def main():
+    print("i, lightshot_url, img_url, img_url_Status , img_host_Status, img_Status")
+    i = 0
 
-while True:
+    while True:
 
-    i += 1
-    print(i, end=", ")
-    ls_url = random_ls_url()
-    print(ls_url, end=", ")
+        i += 1
+        print(i, end=", ")
 
-    ls_res = requests.get(ls_url,  headers={"Accept": "*/*", "User-Agent": "Python 3"})
+        ls_url = random_ls_url()
+        print(ls_url, end=", ")
 
-    img_url = get_img_url(ls_res)
-    print(img_url, end=", ")
+        img_url = get_img_url(requests.get(ls_url,  headers=HEADERS).text)
+        print(img_url, end=", ")
 
-    if not filter_img_url(img_url):
-        print("url_OK", end=", ")
-        img_host_res = requests.get(img_url) # type: ignore
-        if img_host_res.status_code == 200:
-            print("img_Found", end=", ")
-            enc_img = img_host_res.content
-            if not filter_img(enc_img):
-                print("img_OK")
-                # print(i, img_url)
-                img = Image.open(BytesIO(enc_img))
-                # img.show()
+        if not filter_img_url(img_url):
+            print("img_url_OK", end=", ")
 
-                # plt.imshow(img)
-                # plt.show()
+            img_host_res = requests.get(img_url, headers=HEADERS) # type: ignore
 
-                cv2.imshow('image', cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR))
-                cv2.waitKey(2000)
+            if img_host_res.status_code == 200:
+                print("img_Host_200", end=", ")
+
+                enc_img = img_host_res.content
+                
+                if not filter_img(enc_img):
+                    print("img_OK")
+
+                    img = Image.open(BytesIO(enc_img))
+                    # img.show()
+
+                    # plt.imshow(img)
+                    # plt.show()
+
+                    cv2.imshow('image', cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR))
+                    cv2.waitKey(2000)
+
+                else:
+                    print("img_Filtered")
             else:
-                print("img_Filtered")
+                print(f"img_Host_{img_host_res.status_code}, -")
         else:
-            print("img_Not_Found")
-    else:
-        print("url_Filtered")
+            print("img_url_Filtered, -, -")
 
-
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
